c08108f291df7781a9b53ed5766cd8a6db69cfe2
zonewtf <yogrenette@gmail.com>
2026-04-12 19:04:06 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = 'v8'; // <--- Tu changeras juste ça pour la v7, v8, etc.
+const VERSION = 'v9'; // <--- Tu changeras juste ça pour la v7, v8, etc.
 const CACHE = `macros-${VERSION}`;
 
 const ASSETS = [
